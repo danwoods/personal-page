f58cb1d9cf42748d706c7ec49f91d291cc69c185
Dan Woodson <woodson.dan@gmail.com>
2018-04-22 23:12:02 +0100

    Updated meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Dan Woodson',
+    author: 'Dan Woodson',
+    description: 'Personal Site for Dan Woodson',
+    siteUrl: 'https://danwoodson.com/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
