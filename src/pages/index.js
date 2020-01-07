f1e6bc541438aca963e04dad718fc983b3e16ac6
Dan Woodson <woodson.dan@gmail.com>
2020-01-07 14:23:54 -0700

    Updates to match deployed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,42 @@
-import A from "../images/a.png"
-import B from "../images/b.png"
-import C from "../images/c.png"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import Divider from "@material-ui/core/Divider"
-import Favicon from "react-favicon"
-import FilledFavicon from "../images/filled.ico"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import OpenFavicon from "../images/open.ico"
-import React from "react"
-import Typography from "@material-ui/core/Typography"
-import classnames from "classnames"
-import { Helmet } from "react-helmet"
-import { makeStyles, withStyles } from "@material-ui/core/styles"
+import A from '../images/a.png'
+import B from '../images/b.png'
+import C from '../images/c.png'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Divider from '@material-ui/core/Divider'
+import Favicon from 'react-favicon'
+import FilledFavicon from '../images/filled.ico'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import OpenFavicon from '../images/open.ico'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import classnames from 'classnames'
+import { Helmet } from 'react-helmet'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+//import resume from '/dan-woodson_resume.pdf'
 
 // Constants //////////////////////////////////////////////////////////////////
 
 const sectionListStyles = makeStyles({
   root: {
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   item: {
     marginBottom: 0,
   },
 })
 
-
 const SectionList = ({ items }) => {
   const classes = sectionListStyles()
 
   return (
     <List className={classes.root}>
-      {items.map(item => (
+      {items.map((item, idx) => (
         <ListItem dense className={classes.item}>
           <ListItemText
             primary={item}
-            primaryTypographyProps={{ variant: "h6" }}
+            primaryTypographyProps={{ variant: 'h6' }}
           />
         </ListItem>
       ))}
@@ -53,18 +53,18 @@ const aboutSections = [
           margin: 0,
           height: 200,
           width: 150,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          alignSelf: "center",
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          alignSelf: 'center',
         }}
       />
     ),
-    title: "Overview",
+    title: 'Overview',
     TextComponent: () => (
       <SectionList
         items={[
-          "Computer Science Degree",
-          "10+ years Professional Web Development Experience",
+          'Computer Science Degree',
+          '10+ years Professional Web Development Experience',
         ]}
       />
     ),
@@ -77,19 +77,19 @@ const aboutSections = [
           margin: 0,
           height: 200,
           width: 150,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          alignSelf: "center",
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          alignSelf: 'center',
         }}
       />
     ),
-    title: "Areas of Expertise",
+    title: 'Areas of Expertise',
     TextComponent: () => (
       <SectionList
         items={[
-          "Web Application Architecture and Development",
-          "E-commerce solutions",
-          "Websites",
+          'Web Application Architecture and Development',
+          'E-commerce solutions',
+          'Websites',
         ]}
       />
     ),
@@ -102,19 +102,19 @@ const aboutSections = [
           margin: 0,
           height: 200,
           width: 150,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          alignSelf: "center",
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          alignSelf: 'center',
         }}
       />
     ),
-    title: "Personal",
+    title: 'Personal',
     TextComponent: () => (
       <SectionList
         items={[
-          "Married to Jennifer Woodson",
-          "Musician",
-          "Born in Murfreesboro, Tennessee",
+          'Married to Jennifer Woodson',
+          'Musician',
+          'Born in Murfreesboro, Tennessee',
         ]}
       />
     ),
@@ -122,21 +122,18 @@ const aboutSections = [
 ]
 
 // Individual Section /////////////////////////////////////////////////////////
+
 const aboutSectionStyles = makeStyles({
   root: {
-    display: "flex",
+    display: 'flex',
     padding: `${24}px 0`,
-    "&:nth-child(ODD)": {
-      flexDirection: "row-reverse",
-      justifyContent: "space-between",
-    },
   },
   rootReverse: {
-    display: "flex",
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
   },
 })
+
 const AboutSection = ({ ImgComponent, title, TextComponent, idx }) => {
   const classes = aboutSectionStyles()
   return (
@@ -154,8 +151,45 @@ const AboutSection = ({ ImgComponent, title, TextComponent, idx }) => {
           <TextComponent />
         </div>
       </section>
-      <Divider style={{ height: 24, backgroundColor: "black" }} />
+      <Divider style={{ height: 24, backgroundColor: 'black' }} />
     </React.Fragment>
+  )
+}
+
+const linkStyles = makeStyles(theme => ({
+  contactLink: {
+    position: 'fixed',
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+    textDecoration: 'none',
+    [theme.breakpoints.up('md')]: {
+      top: theme.spacing(3),
+      right: theme.spacing(3),
+    },
+  },
+  contactLinkText: {
+    fontFamily: 'courier',
+    fontSize: 11,
+    [theme.breakpoints.up('md')]: {
+      fontSize: 13,
+    },
+  },
+}))
+
+const Link = props => {
+  const classes = linkStyles()
+
+  return (
+    <a
+      href={props.href}
+      className={classnames(classes.contactLink, props.className)}
+      target={'_blank'}
+      title={props.title}
+    >
+      <Typography variant={'caption'} className={classes.contactLinkText}>
+        {`[${props.text}]`}
+      </Typography>
+    </a>
   )
 }
 
@@ -164,31 +198,29 @@ const AboutSection = ({ ImgComponent, title, TextComponent, idx }) => {
 const aboutStyles = makeStyles(theme => ({
   root: {
     maxWidth: theme.spacing(100),
-    width: "100%",
-    margin: "auto",
+    width: '100%',
+    margin: 'auto',
     marginTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
     padding: `0px ${theme.spacing(2)}px`,
   },
   header: {
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       fontSize: theme.spacing(9),
     },
   },
-  contactLink: {
-    position: "fixed",
+  link: {},
+  emailLink: {
     top: theme.spacing(1),
-    right: theme.spacing(1),
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up('md')]: {
       top: theme.spacing(3),
       right: theme.spacing(3),
     },
   },
-  contactLinkText: {
-    fontFamily: "courier",
-    fontSize: 11,
-    [theme.breakpoints.up("md")]: {
-      fontSize: 13,
+  resumeLink: {
+    top: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      top: theme.spacing(5),
     },
   },
 }))
@@ -196,18 +228,20 @@ const aboutStyles = makeStyles(theme => ({
 export const About = props => {
   const classes = aboutStyles()
   const [favicon, setFavicon] = React.useState(0)
+
   React.useEffect(() => {
     setInterval(() => setFavicon(favicon => Number(!Boolean(favicon))), 3500)
   }, [])
+
   return (
     <div>
       <Favicon url={favicon === 1 ? FilledFavicon : OpenFavicon} />
-      <Helmet title={"Dan Woodson"} />
+      <Helmet title={'Dan Woodson'} />
       <CssBaseline />
       <div
         itemscope
         itemtype="http://schema.org/Person"
-        style={{ visibility: "hidden" }}
+        style={{ visibility: 'hidden' }}
       >
         <span itemprop="name" content="Dan Woodson"></span>
         <span itemprop="jobTitle" content="Software Architect"></span>
@@ -227,19 +261,22 @@ export const About = props => {
       </div>
       <main className={classes.root}>
         <Typography variant="h1" className={classes.header} gutterBottom>
-          {"Dan Woodson"}
+          {'Dan Woodson'}
         </Typography>
-        <a
+        <Link
+          className={classes.emailLink}
           href={
-            "mailto:forhire@danwoodson.com?subject=Contacting%20from%20website"
+            'mailto:woodson.dan@gmail.com?subject=Contacting%20from%20website'
           }
-          className={classes.contactLink}
-          target={"_blank"}
-        >
-          <Typography variant={"caption"} className={classes.contactLinkText}>
-            {"CONTACT DAN"}
-          </Typography>
-        </a>
+          text={'contact'}
+          title={'contact'}
+        />
+        <Link
+          className={classes.resumeLink}
+          href={'/dan-woodson_resume.pdf'}
+          text={'resume'}
+          title={'resume'}
+        />
         {aboutSections.map((section, idx) => (
           <AboutSection {...section} idx={idx} />
         ))}
